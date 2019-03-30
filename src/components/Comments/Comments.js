a2f39cdd0e0d3ec62d74cc7a675964921ed2aa3f
sultan <aidaraliuulu@gmail.com>
2019-03-30 19:16:03 +0600

    В компонент Comments.js добавил форму

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,8 +1,19 @@
 import React, {Component, Fragment} from 'react';
 import {connect} from "react-redux";
-import {Button, Card, CardBody, CardText} from "reactstrap";
+import {Button, Card, CardBody, CardText, Col, Form, FormGroup, Input, Label} from "reactstrap";
 
 class Comments extends Component {
+
+    state = {
+        name: '',
+        comment: ''
+    };
+
+    onChangeHandler = event => {
+        const name = event.target.name;
+        this.setState({[name]: event.target.value})
+    };
+
     render() {
         console.log(this.props.comments);
         const news = this.props.comments.news.map(newItem => {
@@ -38,6 +49,45 @@ class Comments extends Component {
                     <hr/>
                     <h2>Comments</h2>
                     {comments}
+                    <hr/>
+                </div>
+                <div>
+                    <h2>Add comment</h2>
+                    <hr/>
+                    <Form onSubmit={this.submitFormHandler}>
+                        <FormGroup>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label sm={2} for="name">Name</Label>
+                            <Col sm={10}>
+                                <Input
+                                    type="text" required
+                                    name="name" id="name"
+                                    placeholder="Enter news name"
+                                    value={this.state.name}
+                                    onChange={this.onChangeHandler}
+                                />
+                            </Col>
+                        </FormGroup>
+
+                        <FormGroup row>
+                            <Label sm={2} for="comment">Content</Label>
+                            <Col sm={10}>
+                                <Input
+                                    type="textarea" required
+                                    name="comment" id="comment"
+                                    placeholder="Enter news comment"
+                                    value={this.state.comment}
+                                    onChange={this.onChangeHandler}
+                                />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Col sm={{offset: 2, size: 10}}>
+                                <Button outline type="submit" color="success">Save</Button>
+                            </Col>
+                        </FormGroup>
+                    </Form>
                 </div>
             </Fragment>
         );
